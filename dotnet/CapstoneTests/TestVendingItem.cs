f396c818648feb79c979ac2fe425d54832d3c44b
Evan Gemmer <evangemmer@gmail.com>
2020-11-02 22:06:22 -0500

    Added additional tests to VendingItem unit test

--- dotnet/CapstoneTests/TestVendingItem.cs
+++ dotnet/CapstoneTests/TestVendingItem.cs
@@ -14,10 +14,19 @@ namespace CapstoneTests
         public void TestMakeNoise()
         {
             VendingItem vendingItem = new VendingItem("A1", "Test Item", 1.50M, "Chip");
+            VendingItem vendingItem2 = new VendingItem("A1", "Test Item", 1.50M, "Candy");
+            VendingItem vendingItem3 = new VendingItem("A1", "Test Item", 1.50M, "Drink");
+            VendingItem vendingItem4 = new VendingItem("A1", "Test Item", 1.50M, "Gum");
 
             string result = vendingItem.MakeNoise();
+            string result2 = vendingItem2.MakeNoise();
+            string result3 = vendingItem3.MakeNoise();
+            string result4 = vendingItem4.MakeNoise();
 
             Assert.AreEqual(" *Crunch Crunch, Yum!*", result);
+            Assert.AreEqual(" *Munch Munch, Yum!*", result2);
+            Assert.AreEqual(" *Glug Glug, Yum!*", result3);
+            Assert.AreEqual(" *Chew Chew, Yum!*", result4);
         }
     }
 }
